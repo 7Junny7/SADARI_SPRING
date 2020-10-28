773a59ecd933ad92873f61993a1baea333ddd5d2
yugangchoe <eqlscl5@naver.com>
2020-10-28 12:28:18 +0900

    아무것도아님

--- src/main/java/co/soft/controller/Test.java
+++ src/main/java/co/soft/controller/Test.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import co.soft.beans.Data1;
 
@@ -18,25 +19,23 @@ public class Test {
 	@Autowired
 	SqlSessionTemplate s;
 	
-	
-	
-	@GetMapping("/index")
+	@RequestMapping("/index")
 	public String index() {
 		return "index";
 	}
 	
-	@GetMapping("/input")
+	@RequestMapping("/input")
 	public String input() {
 		return "input";
 	}
 	
-	@PostMapping("/input1")
+	@RequestMapping("/input1")
 	public String input1(Data1 d1) {
 		s.insert("test.in", d1); //mapper.xml에 있는 것 호출
 		return "input1";
 	}
 	
-	@GetMapping("/result")
+	@RequestMapping("/result")
 	public String result(Model model) {
 		List<Data1> li=s.selectList("test.sel");
 		model.addAttribute("li",li);

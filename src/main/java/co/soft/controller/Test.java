5371d7e676c7988efa560a85980e6c5b2fb3b520
7Junny7 <panthe85@gmail.com>
2020-10-28 12:11:52 +0900

    aa

--- src/main/java/co/soft/controller/Test.java
+++ src/main/java/co/soft/controller/Test.java
@@ -2,13 +2,12 @@ package co.soft.controller;
 
 import java.util.List;
 
-
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import co.soft.beans.Data1;
 
@@ -17,29 +16,27 @@ public class Test {
 
 	@Autowired
 	SqlSessionTemplate s;
-	
-	
-	
-	@GetMapping("/index")
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index() {
 		return "index";
 	}
-	
-	@GetMapping("/input")
+
+	@RequestMapping(value = "/input", method = RequestMethod.GET)
 	public String input() {
 		return "input";
 	}
-	
-	@PostMapping("/input1")
+
+	@RequestMapping(value = "/input1", method = RequestMethod.POST)
 	public String input1(Data1 d1) {
-		s.insert("test.in", d1); //mapper.xml에 있는 것 호출
+		s.insert("test.in", d1); // mapper.xml에 있는 것 호출
 		return "input1";
 	}
-	
-	@GetMapping("/result")
+
+	@RequestMapping(value = "/result", method = RequestMethod.GET)
 	public String result(Model model) {
-		List<Data1> li=s.selectList("test.sel");
-		model.addAttribute("li",li);
+		List<Data1> li = s.selectList("test.sel");
+		model.addAttribute("li", li);
 		return "result";
 	}
 }

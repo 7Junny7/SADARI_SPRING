66ca237247ac19a01e305fbd0f74dde369ae02f3
hyuk0608 <hyeok0608@gmail.com>
2020-10-28 15:52:12 +0900

    userinfobean 뭐라하지

--- src/main/java/co/soft/beans/UserInfoBean.java
+++ src/main/java/co/soft/beans/UserInfoBean.java
@@ -3,10 +3,12 @@ package co.soft.beans;
 public class UserInfoBean {
 
 	//유저 정보, 로그인 및 회원가입 용, 회원정보 및 평가 용
-	private String id;
-	private String password;
-	private int userLikeScore; //좋아요 점수
-	private int userLikePerson; //투표 인원수  -> 두개 조합으로 좋아요 rate(별점) 계산
+	private String userId;
+	private String userPassword;
+	private int userBirth;
+	private int userPhone;
+	private int userTemp;
+	private int userLikeUser; //투표 인원수  -> 두개 조합으로 좋아요 rate(별점) 계산
 	private String favorite; //선호 음식
 	private String userComment; //회원 평가
 }
